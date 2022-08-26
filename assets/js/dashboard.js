ad3219a120a579a46cf55f79a7d65dc5edb20361
MattP013 <mmateus20pereira@gmail.com>
2022-08-25 22:45:05 -0300

    iniciando formulario de cadastro

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -48,12 +48,34 @@ $(document).ready(function () {
 
   function ProductById(Id) {
     const Produto = Produtos.find((element) => {
-      return element.id == Id
-    })
-    
-    $(".descricao").text(Produto.descricao)
+      return element.id == Id;
+    });
 
+    const src = window.location.origin;
+    let inputs = $('.updateProduct').find('input')
+    let textarea = $('.updateProduct').find('textarea')
+
+    console.log(inputs);
+    $(inputs[1]).val(Produto.nome);
+    $(inputs[2]).val(Produto.preco);
+    $(inputs[3]).val(Produto.categoria);
+    $(textarea).val(Produto.descricao);
+   
+    $("#product-img").attr("src", `${src}/assets/img/${Produto.img}`);
   }
+
+  $("input[type='file']").on('change', function (){
+    if(this.files && this.files[0])
+    {
+      let file = new FileReader();
+      file.onload = function (e){
+        $(".product-image").find('img').attr("src", e.target.result);
+      }
+      file.readAsDataURL(this.files[0])
+    }
+  })
+
+
   const Table = $(".produtos").children("tbody");
 
   function LoadTable(Produtos, Table) {
@@ -64,8 +86,8 @@ $(document).ready(function () {
             <th scope="row">${element.id}</th>
             <td>${element.nome}</td>
             <td>${Intl.NumberFormat("pt-br", {
-        minimumFractionDigits: 2,
-      }).format(element.preco)}</td>
+              minimumFractionDigits: 2,
+            }).format(element.preco)}</td>
             <td>${element.qtd}</td>
             <td class="d-md-block d-none">
               <i
@@ -84,16 +106,40 @@ $(document).ready(function () {
 
   LoadTable(Produtos, Table);
 
-
-
   $(".fa-edit").click(function (e) {
     const produto = $(this).parents("tr").find("th").text();
-    ProductById(produto)
+    ProductById(produto);
   });
 
   $(".fa-times-circle").click(function (e) {
     $(this).parents("tr").remove();
   });
 
+  var input = $('input[name="categoria_produto"]');
+  var categorias = $("#categorias");
+  var option = $("option");
 
+  $(input).on("focus", function () {
+    categorias.css("display", "block");
+    input.css("border-radius", "5px 5px 0 0");
+  });
+
+  $(option).click(function () {
+    input.val($(this).val());
+    categorias.css("display", "none");
+    input.css("border-radius", "5px");
+  });
+
+  $(input).on("input", function () {
+    var text = input.val().toUpperCase();
+    var aux = categorias.find(option).val().toUpperCase().indexOf(text);
+
+    for (let element of option) {
+      if ($(element).val().toUpperCase().indexOf(text) > -1) {
+        $(element).css("display", "block");
+      } else {
+        $(element).css("display", "none");
+      }
+    }
+  });
 });
