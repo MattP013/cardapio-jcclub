17a24e17cf0b5dcdff9aa460faa5bbee18916dd6
MattP013 <mmateus20pereira@gmail.com>
2022-09-03 23:17:26 -0300

    ajustes de layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ function carregarCardapio(cardapio) {
                     <div class="acao d-flex justify-content-between align-items-end">
                         <span class="preco">R$ ${Intl.NumberFormat('pt-br', { minimumFractionDigits: 2 }).format(c.preco)}</span>
                         <button data-cid="${c.id}" class="adicionar">
-                            +
+                            <i class="fas fa-plus"></i>
                         </button>
                     </div>
                 </div>
@@ -163,12 +163,12 @@ function carregarStorage(Cproduto) {
                 </div>
                 <div class="ms-2 w-100">
                     <h6 class="nome-produto">
-                        ${c[0].nome} <span class="excluir" onclick="excluirProduto(${index})">X</span>
+                        ${c[0].nome} <span class="excluir" onclick="excluirProduto(${index})"><i class="fas fa-times"></i></span>
                     </h6>
                     <p class="descricao-produto">
                         ${c[0].descricao}
                     </p>
-                    <div data-cid="${c[0].id}" class="operacao mt-2 d-flex justify-content-md-start gap-md-3 gap-0 justify-content-between">
+                    <div data-cid="${c[0].id}" class="operacao mt-2 d-flex justify-content-between">
                             ${botaoDiminuir(c[0].qtd, index)}
                             <span class="preco align-self-center">
                                 x${c[0].qtd} - R$ ${carregarPreco(c[0].preco, c[0].qtd, index)}
@@ -185,6 +185,7 @@ function carregarStorage(Cproduto) {
     {
         ContadorProdutos(contarprodutos)
         containerCarrinho.html(retorno)
+        $('.enviar-whatsapp').attr('disabled',false)
         
     }
     else
@@ -225,6 +226,8 @@ function resetCar()
 {
     $('.contador').addClass('d-none')
     $('.container-total').addClass('d-none')
+    $('.enviar-whatsapp').attr('disabled',true)
+
     containerCarrinho.html(` <p class="text-center" style="font-size: 20px;">Nenhum produto selecionado</p>`)
 }
 
@@ -289,8 +292,16 @@ $(document).ready(function () {
 
 
         }
-            
+    })
 
+    $('.enviar-whatsapp').click(function(){
+        let Produtos = lerStorage()
+
+        const ProdutosParaEnvio = Produtos.map((P)=>{
+            return `${P[0].qtd}x ${P[0].nome} \n`
+        })
+        const Mensagem = window.encodeURIComponent("Salve!! Segue a lista de produtos:\n " + ProdutosParaEnvio)
+        window.open("https://api.whatsapp.com/send?phone=5513991764271&text=" + Mensagem, "__blank")
     })
 
 })
